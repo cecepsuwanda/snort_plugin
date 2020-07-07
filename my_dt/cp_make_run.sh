aef1b95afca0ada8c286c92ed6f5d179d3212e4a
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2020-07-07 07:03:42 +0700

    Update cp_make_run.sh

--- my_dt/cp_make_run.sh
+++ my_dt/cp_make_run.sh
@@ -6,4 +6,4 @@ cp -rf /mnt/e/snort_plugin/my_dt/Tdataframe.* .
 cp -rf /mnt/e/snort_plugin/my_dt/Tdec_tree.* .
 cp -rf /mnt/e/snort_plugin/my_dt/main.cc .
 make
-./my_dt 15 data/kddcup.names data/train_full_corrected.csv data/test_full_correctd.csv data/dc_full_tree_dp15.csv 
+./my_dt 5 data/kddcup.names data/train_full_corrected.csv data/test_full_correctd.csv data/dc_full_tree_dp5.csv 
