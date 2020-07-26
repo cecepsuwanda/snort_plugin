6af7786f22f4aa2c5525f9e928d3c0168b3a50a7
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2020-07-27 05:46:37 +0700

    my_dt mit_darpa update

--- mit_darpa_99/Tread_file.cc
+++ mit_darpa_99/Tread_file.cc
@@ -10,6 +10,8 @@ Tread_file::~Tread_file()
 {
   clear_data();
   close_file();
+  clear_index();
+  clear_memory();
 }
 
 Tread_file::Tread_file(const Tread_file &t)
@@ -29,6 +31,12 @@ void Tread_file::setseparator(const char* separator)
   _separator = separator;
 }
 
+void Tread_file::file_map()
+{
+  is_fmap = true;
+  //cout << "is_fmap " << is_fmap << endl;
+}
+
 bool Tread_file::open_file(string mode)
 {
   _file = fopen(_nm_f.c_str(), (char *) mode.c_str());
@@ -37,16 +45,41 @@ bool Tread_file::open_file(string mode)
 
 bool Tread_file::open_file()
 {
-  _file = fopen(_nm_f.c_str(), "r");
-  return !(_file == NULL);
+  if (is_fmap)
+  {
+    _fd = open(_nm_f.c_str(), O_RDONLY, S_IRUSR | S_IWUSR);
+
+    if (fstat(_fd, &_sb) == -1)
+    {
+      perror("couldn't get file size. \n");
+    }
+
+    //printf("file size is %ld\n",_sb.st_size);
+
+    _file_in_memory = (char*) mmap(NULL, _sb.st_size, PROT_READ, MAP_PRIVATE, _fd, 0);
+    _posisi = 0;
+    _idx_posisi = 0;
+
+    return !(_fd == -1);
+  } else {
+    _file = fopen(_nm_f.c_str(), "r");
+    return !(_file == NULL);
+  }
 }
 
 void Tread_file::close_file()
 {
-  if (!(_file == NULL))
+  if (is_fmap)
   {
-    fclose(_file);
-    _file = NULL;
+    munmap(_file_in_memory, _sb.st_size);
+    close(_fd);
+  } else {
+
+    if (!(_file == NULL))
+    {
+      fclose(_file);
+      _file = NULL;
+    }
   }
 }
 
@@ -71,13 +104,54 @@ vector<string> Tread_file::tokenizer(char * str, const char* separator)
 
 void Tread_file::read_file()
 {
-  char str[1000];
   clear_data();
-
-  if (fgets(str, 1000, _file) != NULL)
+  char str[1000];
+  if (is_fmap)
   {
+    strcpy(str, "");
+
+    if (is_index and (_jml_index > 0))
+    {      
+      int p_awal = _idx_in_memory[_idx_posisi];
+      while (( p_awal < _sb.st_size) and (_file_in_memory[p_awal] != '\n') )
+      {
+        char tmp = _file_in_memory[p_awal];
+        strncat(str, &tmp, 1);
+        p_awal++;
+      }
+      char tmp = _file_in_memory[p_awal];
+      strncat(str, &tmp, 1);
+      _idx_posisi++;
+
+    } else {
+
+      _b_posisi = _posisi;
+      while (( _posisi < _sb.st_size) and (_file_in_memory[_posisi] != '\n') )
+      {
+        char tmp = _file_in_memory[_posisi];
+        strncat(str, &tmp, 1);
+        _posisi++;
+      }
+      char tmp = _file_in_memory[_posisi];      
+      strncat(str, &tmp, 1);
+      
+      // if(_b_posisi==0)
+      //    cout << str << endl;
+      
+      _posisi++;
+    }
     _data = tokenizer(str, _separator);
+    //cout << "Hasil Token : " << _data.size() <<endl;
+  } else {
+
+    if (fgets(str, 1000, _file) != NULL)
+    {
+      //cout << str << endl;
+      _data = tokenizer(str, _separator);
+    }
   }
+
+
 }
 
 void Tread_file::write_file(string row)
@@ -92,10 +166,29 @@ void Tread_file::clear_data()
   _data.shrink_to_fit();
 }
 
+void Tread_file::clear_index()
+{
+  _index.clear();
+  _index.shrink_to_fit();
+}
+
 
 bool Tread_file::is_eof()
 {
-  return feof(_file);
+  if (is_fmap)
+  {
+    if (is_index)
+    {
+      return (_idx_posisi > _jml_index);
+    }
+    else
+    {
+      return (_posisi > _sb.st_size);
+    }
+
+  } else {
+    return feof(_file);
+  }
 
 }
 
@@ -107,4 +200,45 @@ vector<string> Tread_file::get_record()
 void Tread_file::next_record()
 {
   read_file();
+}
+
+void Tread_file::index_on()
+{
+  is_index = true;
+  //cout << "is_index on " << is_index << endl;
+}
+
+void Tread_file::index_off()
+{
+  is_index = false;
+  //cout << "is_index off " << is_index << endl;
+}
+
+void Tread_file::add_index()
+{
+  _index.push_back(_b_posisi);
+}
+
+void Tread_file::save_to_memory()
+{
+
+  _jml_index = _index.size();
+  _ukuran_index = (((_jml_index * sizeof(int)) / pagesize) + 1) * pagesize;
+
+  _idx_in_memory = (int*) mmap(NULL, _ukuran_index, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+  for (int i = 0; i < _jml_index; ++i)
+  {
+    _idx_in_memory[i] = _index[i];
+  }
+}
+
+void Tread_file::clear_memory()
+{
+  if(_idx_in_memory!=NULL){  
+    munmap(_idx_in_memory, _ukuran_index);
+    _idx_in_memory = NULL;
+    _ukuran_index = 0;
+    _jml_index = 0;
+  }  
 }
--- mit_darpa_99/Tread_file.h
+++ mit_darpa_99/Tread_file.h
@@ -3,20 +3,48 @@
 #include <cstring>
 #include <vector>
 #include <string>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>     /* general SysV IPC structures          */
+#include <sys/shm.h>	 /* shared memory functions and structs. */
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#define pagesize  4096
 
 class Tread_file {
 private:
 	string _nm_f;
 	const char *_separator;
 	FILE *_file = NULL;
+
 	vector<string> _data;
 
+	bool is_fmap = false;
+	int  _posisi = 0;
+	int  _b_posisi = 0;
+	int _fd = -1;
+	char * _file_in_memory = NULL;
+	struct stat _sb;
+
+	int _idx_posisi = 0;
+	bool is_index = false;
+	vector<int> _index;
+	int *_idx_in_memory = NULL;
+	int _jml_index = 0;
+	int _ukuran_index = 0;
+
 	vector<string> tokenizer(char* str, const char* separator);
-
-
 	void clear_data();
+
 public:
 	Tread_file();
 	~Tread_file();
@@ -30,7 +58,7 @@ public:
 
 	void setnm_f(string nm_f);
 	void setseparator(const char* separator);
-
+	void file_map();
 
 	bool open_file();
 	bool open_file(string mode);
@@ -41,5 +69,12 @@ public:
 	bool is_eof();
 	void next_record();
 	vector<string> get_record();
+
+	void clear_index();
+	void index_on();
+	void index_off();
+	void add_index();
+	void save_to_memory();
+	void clear_memory();
 
 };
