0b275abd6ce33f07cf5f1c64d91bd045bce22ccc
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2020-09-05 06:34:32 +0700

    Update cp_make_run.sh

--- mit_darpa_99/cp_make_run.sh
+++ mit_darpa_99/cp_make_run.sh
@@ -8,40 +8,60 @@ cp -rf /mnt/e/snort_plugin/mit_darpa_99/Tconf_metrix.* .
 cp -rf /mnt/e/snort_plugin/mit_darpa_99/main.cc .
 make
 
-./labeling data/master_identifications.list ../my_dt/data/4w_friday_out.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_friday_in.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_monday_out.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_monday_in.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_thursday_out.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_thursday_in.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_tuesday_out.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_wednesday_out.csv
-wait
-./labeling data/master_identifications.list ../my_dt/data/4w_wednesday_in.csv
-wait
-# ./labeling data/master_identifications.list ../my_dt/data/friday.csv
+# ./labeling data/master_identifications.list ../my_dt/data/4w_friday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_friday_in.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_monday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_monday_in.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_thursday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_thursday_in.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_tuesday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_wednesday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/4w_wednesday_in.csv
+# wait
+# # ./labeling data/master_identifications.list ../my_dt/data/friday.csv
 
-./labeling data/master_identifications.list ../my_dt/data/5w_friday_in.csv
+# ./labeling data/master_identifications.list ../my_dt/data/5w_friday_in.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_monday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_monday_in.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_thursday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_thursday_in.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_tuesday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_tuesday_in.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_wednesday_out.csv
+# wait
+# ./labeling data/master_identifications.list ../my_dt/data/5w_wednesday_in.csv
+
+./labeling data/master_identifications.list ../my_dt/data/2w_friday_out.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_monday_out.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_friday_in.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_monday_in.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_monday_out.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_thursday_out.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_monday_in.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_thursday_in.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_thursday_out.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_tuesday_out.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_thursday_in.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_tuesday_in.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_tuesday_out.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_wednesday_out.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_tuesday_in.csv
 wait
-./labeling data/master_identifications.list ../my_dt/data/5w_wednesday_in.csv
+./labeling data/master_identifications.list ../my_dt/data/2w_wednesday_out.csv
+wait
+./labeling data/master_identifications.list ../my_dt/data/2w_wednesday_in.csv
