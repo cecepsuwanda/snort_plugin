ebc7ed4d70a9a04c4b8443971a60abcb5f482d59
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2021-02-22 07:58:36 +0700

    Create download.sh

---
+++ mit_darpa_99/download.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+cd ~/Dataset/mit_darpa_99
+
+cd ~/Dataset/mit_darpa_99/first_week/monday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/monday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/monday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
+
+cd ~/Dataset/mit_darpa_99/first_week/tuesday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/tuesday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/tuesday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
+
+cd ~/Dataset/mit_darpa_99/first_week/wednesday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/wednesday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/wednesday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
+
+cd ~/Dataset/mit_darpa_99/first_week/friday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/friday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week1/friday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
+
+
+cd ~/Dataset/mit_darpa_99/third_week/monday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/monday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/monday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
+
+cd ~/Dataset/mit_darpa_99/third_week/tuesday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/tuesday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/tuesday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
+
+cd ~/Dataset/mit_darpa_99/third_week/wednesday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/wednesday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/wednesday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
+
+cd ~/Dataset/mit_darpa_99/third_week/friday
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/friday/outside.tcpdump.gz --no-check-certificate
+wget https://archive.ll.mit.edu/ideval/data/1999/training/week3/friday/inside.tcpdump.gz --no-check-certificate
+gunzip outside.tcpdump.gz
+gunzip inside.tcpdump.gz
