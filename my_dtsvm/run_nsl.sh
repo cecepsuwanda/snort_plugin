b9091ea8aa8f3871735ff106a63fbbe5b3cf2cc5
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2021-03-30 07:47:09 +0700

    my_dtsvm update

--- my_dtsvm/run_nsl.sh
+++ my_dtsvm/run_nsl.sh
@@ -8,83 +8,83 @@ mkdir model/model2
 mkdir model/model3
 mkdir model/model4
 
-mkdir ~/Dataset/NSL/model_100_2_svm_30
+mkdir ~/Dataset/NSL/model_100_2_svm_30_credal05
 
-./my_dtsvm 0 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model1 0 0 0 | tee ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 0 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model1 0 0 1 | tee ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model1 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 1 1 0 0 100 2 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model1 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model1 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 1 1 0 0 100 2 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model1 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait 
 
-./my_dtsvm  1 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model1 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt 
+./my_dtsvm  1 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model1 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt 
 wait
 
-./my_dtsvm  1 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model1 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm  1 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model1 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
 
-./my_dtsvm 2 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model2 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt 
+./my_dtsvm 2 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model2 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt 
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model2 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 1 1 0 0 100 2 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model2 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model2 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt 
+./my_dtsvm 1 1 0 0 100 2 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model2 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt 
 wait 
 
-./my_dtsvm  1 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model2 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm  1 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model2 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm  1 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model2 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm  1 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model2 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
 
 
-./my_dtsvm 2 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model3 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt 
+./my_dtsvm 2 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model3 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt 
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model3 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 1 1 0 0 100 2 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model3 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model3 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 1 1 0 0 100 2 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model3 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait 
 
-./my_dtsvm  1 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model3 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm  1 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model3 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm  1 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model3 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm  1 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model3 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
 
 
-./my_dtsvm 2 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model4 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt 
+./my_dtsvm 2 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model4 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt 
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model4 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 1 1 0 0 100 2 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model model4 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm 1 1 0 0 100 2 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model4 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm 1 1 0 0 100 2 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model model4 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait 
 
-./my_dtsvm  1 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model4 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm  1 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model model4 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
-./my_dtsvm  1 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model4 0 0 0 | tee -a ~/Dataset/NSL/model_100_2_svm_30/hasil.txt
+./my_dtsvm  1 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model model4 0 0 1 | tee -a ~/Dataset/NSL/model_100_2_svm_30_credal05/hasil.txt
 wait
 
 
 
-rm -rf ~/Dataset/NSL/model_100_2_svm_30/model*
-rm -rf ~/Dataset/NSL/model_100_2_svm_30/test
-rm -rf ~/Dataset/NSL/model_100_2_svm_30/train
-rm -rf ~/Dataset/NSL/model_100_2_svm_30/dtsvm_model.csv
+rm -rf ~/Dataset/NSL/model_100_2_svm_30_credal05/model*
+rm -rf ~/Dataset/NSL/model_100_2_svm_30_credal05/test
+rm -rf ~/Dataset/NSL/model_100_2_svm_30_credal05/train
+rm -rf ~/Dataset/NSL/model_100_2_svm_30_credal05/dtsvm_model.csv
 
-cp -rf model/model* ~/Dataset/NSL/model_100_2_svm_30
-cp -rf model/train ~/Dataset/NSL/model_100_2_svm_30
-cp -rf model/test ~/Dataset/NSL/model_100_2_svm_30
-cp -rf model/dtsvm_model.csv ~/Dataset/NSL/model_100_2_svm_30
+cp -rf model/model* ~/Dataset/NSL/model_100_2_svm_30_credal05
+cp -rf model/train ~/Dataset/NSL/model_100_2_svm_30_credal05
+cp -rf model/test ~/Dataset/NSL/model_100_2_svm_30_credal05
+cp -rf model/dtsvm_model.csv ~/Dataset/NSL/model_100_2_svm_30_credal05
 
 
