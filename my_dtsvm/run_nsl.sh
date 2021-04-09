7ff01a7a7967ebf46f5c7d6565a2693766b30106
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2021-04-09 10:57:00 +0700

    Update run_nsl.sh

--- my_dtsvm/run_nsl.sh
+++ my_dtsvm/run_nsl.sh
@@ -1,104 +1,104 @@
 #!/bin/bash
 
-# mkdir model/model_100_124_svm_credal1
-# mkdir model/model_100_124_svm_credal1/test
-# mkdir model/model_100_124_svm_credal1/train
-# mkdir model/model_100_124_svm_credal1/model1
-# mkdir model/model_100_124_svm_credal1/model2
-# mkdir model/model_100_124_svm_credal1/model3
-# mkdir model/model_100_124_svm_credal1/model4
+mkdir model/model_100_2_svm
+mkdir model/model_100_2_svm/test
+mkdir model/model_100_2_svm/train
+mkdir model/model_100_2_svm/model1
+mkdir model/model_100_2_svm/model2
+mkdir model/model_100_2_svm/model3
+mkdir model/model_100_2_svm/model4
 
-# rm -rf model/model_100_124_svm_credal1/hasil.txt
-# rm -rf model/model_100_124_svm_credal1/dtsvm_model.csv
-# rm -rf model/model_100_124_svm_credal1/all_metrik.csv
-# rm -rf model/model_100_124_svm_credal1/dt_metrik.csv
-# rm -rf model/model_100_124_svm_credal1/svm_metrik.csv
-# rm -rf model/model_100_124_svm_credal1/dtsvm_metrik.csv
-
-
-# ./my_dtsvm 0 1 0 0 100 124 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model1 0 0 1 1.0 | tee model/model_100_124_svm_credal1/hasil.txt
-# wait
-
-# ./my_dtsvm 1 1 0 0 100 124 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model1 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
-
-# ./my_dtsvm 1 1 0 0 100 124 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_124_svm_credal1 model1 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait 
-
-# ./my_dtsvm  1 1 0 0 100 124 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_124_svm_credal1 model1 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt 
-# wait
-
-# ./my_dtsvm  1 1 0 0 100 124 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_124_svm_credal1 model1 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+rm -rf model/model_100_2_svm/hasil.txt
+rm -rf model/model_100_2_svm/dtsvm_model.csv
+rm -rf model/model_100_2_svm/all_metrik.csv
+rm -rf model/model_100_2_svm/dt_metrik.csv
+rm -rf model/model_100_2_svm/svm_metrik.csv
+rm -rf model/model_100_2_svm/dtsvm_metrik.csv
 
 
-# ./my_dtsvm 2 1 0 0 100 124 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model2 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt 
-# wait
+./my_dtsvm 0 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model1 0 0 0 0.0 | tee model/model_100_2_svm/hasil.txt
+wait
 
-# ./my_dtsvm 1 1 0 0 100 124 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model2 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm 1 1 0 0 100 2 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model1 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
-# ./my_dtsvm 1 1 0 0 100 124 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_124_svm_credal1 model2 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt 
-# wait 
+./my_dtsvm 1 1 0 0 100 2 0.0001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_2_svm model1 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait 
 
-# ./my_dtsvm  1 1 0 0 100 124 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_124_svm_credal1 model2 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm  1 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_2_svm model1 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt 
+wait
 
-# ./my_dtsvm  1 1 0 0 100 124 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_124_svm_credal1 model2 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm  1 1 0 0 100 2 0.0001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_2_svm model1 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
 
+./my_dtsvm 2 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model2 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt 
+wait
 
-# ./my_dtsvm 2 1 0 0 100 124 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model3 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt 
-# wait
+./my_dtsvm 1 1 0 0 100 2 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model2 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
-# ./my_dtsvm 1 1 0 0 100 124 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model3 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm 1 1 0 0 100 2 0.0003 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_2_svm model2 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt 
+wait 
 
-# ./my_dtsvm 1 1 0 0 100 124 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_124_svm_credal1 model3 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait 
+./my_dtsvm  1 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_2_svm model2 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
-# ./my_dtsvm  1 1 0 0 100 124 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_124_svm_credal1 model3 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
-
-# ./my_dtsvm  1 1 0 0 100 124 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_124_svm_credal1 model3 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm  1 1 0 0 100 2 0.0003 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_2_svm model2 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
 
 
-# ./my_dtsvm 2 1 0 0 100 124 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model4 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt 
-# wait
+./my_dtsvm 2 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model3 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt 
+wait
 
-# ./my_dtsvm 1 1 0 0 100 124 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_124_svm_credal1 model4 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm 1 1 0 0 100 2 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model3 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
-# ./my_dtsvm 1 1 0 0 100 124 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_124_svm_credal1 model4 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait 
+./my_dtsvm 1 1 0 0 100 2 0.0005 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_2_svm model3 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait 
 
-# ./my_dtsvm  1 1 0 0 100 124 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_124_svm_credal1 model4 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm  1 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_2_svm model3 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
-# ./my_dtsvm  1 1 0 0 100 124 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_124_svm_credal1 model4 0 0 1 1.0 | tee -a model/model_100_124_svm_credal1/hasil.txt
-# wait
+./my_dtsvm  1 1 0 0 100 2 0.0005 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_2_svm model3 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
 
-mkdir ~/Dataset/NSL/model_100_124_svm_credal1
 
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/model*
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/test
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/train
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/dtsvm_model.csv
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/hasil.txt
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/all_metrik.csv
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/dt_metrik.csv
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/svm_metrik.csv
-rm -rf ~/Dataset/NSL/model_100_124_svm_credal1/dtsvm_metrik.csv
 
-cp -rf model/model_100_124_svm_credal1/model* ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/train ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/test ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/dtsvm_model.csv ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/hasil.txt ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/all_metrik.csv ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/dt_metrik.csv ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/svm_metrik.csv ~/Dataset/NSL/model_100_124_svm_credal1
-cp -rf model/model_100_124_svm_credal1/dtsvm_metrik.csv ~/Dataset/NSL/model_100_124_svm_credal1
+./my_dtsvm 2 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model4 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt 
+wait
+
+./my_dtsvm 1 1 0 0 100 2 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_train.txt model/model_100_2_svm model4 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
+
+./my_dtsvm 1 1 0 0 100 2 0.001 0.01 data/kddcupSVM_33attr.names data/NSLSVM_33attr_test.txt model/model_100_2_svm model4 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait 
+
+./my_dtsvm  1 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_test1.txt model/model_100_2_svm model4 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
+
+./my_dtsvm  1 1 0 0 100 2 0.001 0.01  data/kddcupSVM_33attr.names data/NSLSVM_33attr_unknown.txt model/model_100_2_svm model4 0 0 0 0.0 | tee -a model/model_100_2_svm/hasil.txt
+wait
+
+mkdir ~/Dataset/NSL/model_100_2_svm
+
+rm -rf ~/Dataset/NSL/model_100_2_svm/model*
+rm -rf ~/Dataset/NSL/model_100_2_svm/test
+rm -rf ~/Dataset/NSL/model_100_2_svm/train
+rm -rf ~/Dataset/NSL/model_100_2_svm/dtsvm_model.csv
+rm -rf ~/Dataset/NSL/model_100_2_svm/hasil.txt
+rm -rf ~/Dataset/NSL/model_100_2_svm/all_metrik.csv
+rm -rf ~/Dataset/NSL/model_100_2_svm/dt_metrik.csv
+rm -rf ~/Dataset/NSL/model_100_2_svm/svm_metrik.csv
+rm -rf ~/Dataset/NSL/model_100_2_svm/dtsvm_metrik.csv
+
+cp -rf model/model_100_2_svm/model* ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/train ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/test ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/dtsvm_model.csv ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/hasil.txt ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/all_metrik.csv ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/dt_metrik.csv ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/svm_metrik.csv ~/Dataset/NSL/model_100_2_svm
+cp -rf model/model_100_2_svm/dtsvm_metrik.csv ~/Dataset/NSL/model_100_2_svm
