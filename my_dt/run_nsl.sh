c4c40dde08d51a7239575471fcc60c4e5fe3cc0d
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2022-10-02 01:43:43 +0700

    update

--- my_dt/run_nsl.sh
+++ my_dt/run_nsl.sh
@@ -8,7 +8,7 @@ TEST='Dataset/NSL/tree_dataset/19052021_0050/NSLTree_29attr_test.txt'
 DEPTH=100
 MIN_SAMP=100
 USE_CREDAL=1
-CREDAL_S=0.5
+CREDAL_S=1.0
 LIMIT=1
 THRESHOLD=2
 
@@ -25,7 +25,7 @@ rm -rf ~/$PATH_MODEL/dtsvm_metrik.csv
 START="$(date +"%r")"
 echo "START : $START"
 
-./my_dt 0 $DEPTH $MIN_SAMP $USE_CREDAL $CREDAL_S $LIMIT $THRESHOLD ~/$NAMES ~/$TRAIN ~/$TEST ~/$PATH_MODEL | tee ~/$PATH_MODEL/hasil.txt
+./my_dt 0 $DEPTH $MIN_SAMP $USE_CREDAL $CREDAL_S $LIMIT $THRESHOLD ~/$NAMES ~/$TRAIN ~/$TEST ~/$PATH_MODEL | tee ~/$PATH_MODEL/hasil_credal1.txt
 wait
 
 END="$(date +"%r")"
