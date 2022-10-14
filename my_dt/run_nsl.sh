617a1f6e8c532258185f2579f055d9c7e6ca214b
Cecep Suwanda <Cecepsuwanda@yahoo.com>
2022-10-14 12:50:26 +0700

    update

--- my_dt/run_nsl.sh
+++ my_dt/run_nsl.sh
@@ -6,11 +6,11 @@ TRAIN='Dataset/NSL/tree_dataset/19052021_0050/NSLTree_29attr_train.txt'
 TEST='Dataset/NSL/tree_dataset/19052021_0050/NSLTree_29attr_test.txt'
 
 DEPTH=2
-MIN_SAMP=2
+MIN_SAMP=20
 USE_CREDAL=0
 CREDAL_S=0.0
 LIMIT=1
-THRESHOLD=2
+THRESHOLD=10
 
 
 mkdir ~/$PATH_MODEL
@@ -25,7 +25,7 @@ rm -rf ~/$PATH_MODEL/dtsvm_metrik.csv
 START="$(date +"%r")"
 echo "START : $START"
 
-./my_dt 0 $DEPTH $MIN_SAMP $USE_CREDAL $CREDAL_S $LIMIT $THRESHOLD ~/$NAMES ~/$TRAIN ~/$TEST ~/$PATH_MODEL | tee ~/$PATH_MODEL/coba_S2_T2_prunning.txt
+./my_dt 0 $DEPTH $MIN_SAMP $USE_CREDAL $CREDAL_S $LIMIT $THRESHOLD ~/$NAMES ~/$TRAIN ~/$TEST ~/$PATH_MODEL | tee ~/$PATH_MODEL/coba_S20_T10_prunning.txt
 wait
 
 END="$(date +"%r")"
